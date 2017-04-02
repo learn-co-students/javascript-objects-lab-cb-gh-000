9134882c1a25f76ae207975fc51dbe5b70ea4785
Leon Leong <leonleonglilong@gmail.com>
2017-04-02 12:15:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+var recipes = {};
+
+
+function updateObjectWithKeyAndValue(object, key, value) {
+
+  return Object.assign({}, object, {[key]: value});
+
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+
+  object[key] = value;
+
+  return object;
+  
+}
+
+function deleteFromObjectByKey(object, key) {
+
+  var myObj = Object.assign({}, recipes);
+
+  delete myObj[key];
+
+  return myObj;
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+
+  delete object[key];
+
+  return object;
+  
+}
+
