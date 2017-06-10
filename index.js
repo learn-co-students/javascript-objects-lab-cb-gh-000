4010cdf8ee829da163b53c970f422838fd2ffe17
Yamini  Gahlot <yaminidm@gmail.com>
2017-06-10 13:05:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-var playlist = new Object({artist: "name"})
+var recipes = new Object({Ind: 'chicken'})
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var res = Object.assign({}, object, { [key]: value})
+  return res
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  Object.assign(object, { [key]: value})
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var res = Object.assign({}, object)
+  delete res.prop;
+  return res
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.prop;
+  return object
+}
