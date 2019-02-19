3902d7f88ade54b45534973c0aaa3b5613cdb0fd
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-02-19 14:56:45 +0000

    Done.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key) {
-  return Object.assign(delete object.key, {})
+  return Object.assign({}, delete object.key);
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
