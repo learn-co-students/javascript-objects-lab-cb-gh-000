57e8f2e4c83b173a89b5bcffbda987d8476d586b
Jordan de Souza <ja3desou@uwaterloo.ca>
2019-06-07 14:52:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {};
 
-function updateObjectWithKeyAndValue(object, key, value){
+//function updateObjectWithKeyAndValue(object, key, value){
   var new = Object.assign({key: value}, object);
   return new
 }
